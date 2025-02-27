8d4ced6e9f084af1f7122e97d42d300f10b65bcb
hariharan <hharan987@gmail.com>
2025-02-27 22:08:59 +0800

    revert config

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { connectDB } = require("./src/config/db");
 const mainRouter = require("./src/routes/mainRouter");
 const app = express();
 dotenv.config();
-// const PORT = process.env.APP_PORT;
+const PORT = process.env.APP_PORT;
 
 // Enable CORS for all routes
 app.use(cors());
@@ -16,4 +16,6 @@ app.use(express.json({limit: "50mb"}));
 // api-routes
 app.use("/harvest-hub",mainRouter);
 
-module.exports = app;
+app.listen(PORT,() => {
+    console.log(`harvest hub server running on port ${PORT}`);
+});
