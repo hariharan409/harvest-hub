f16ba8176f2c18dc13435a9ad2f09322ba45f563
hariharan <hharan987@gmail.com>
2025-03-04 18:02:52 +0800

    impl swagger setup

--- server/src/sub-routes/dashboardRouter.js
+++ server/src/sub-routes/dashboardRouter.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express.Router();
 const dashboardController = require("../controllers/dashboardController");
 
-/* get request */
+/**
+ * @swagger
+ * /harvest-hub/dashboard/get-current-weather:
+ *   get:
+ *     summary: Get current weather report
+ *     tags: [Dashboard]
+ *     responses:
+ *       200:
+ *         description: success
+ */
+
 app.get("/get-current-weather",dashboardController.getWeatherReport);
 app.get("/get-current-wind",dashboardController.getWindReport);
 app.get("/get-today-forecast",dashboardController.getTodayForecast);
