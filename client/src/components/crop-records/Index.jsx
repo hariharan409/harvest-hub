4e7edb457839210a407ac7f80e3bf0afdcab9bd7
hariharan <hharan987@gmail.com>
2025-03-05 15:46:03 +0800

    add responsive tothe crop record table

--- client/src/components/crop-records/Index.jsx
+++ client/src/components/crop-records/Index.jsx
@@ -15,15 +15,15 @@ const CropRecords = () => {
     return(
         <div>
             {/* row 1 */}
-            <div className="flex justify-between">
+            <div className="flex flex-col sm:flex-row justify-between items-center">
                 <div class="inline-flex rounded-md shadow-xs mb-2" role="group">
-                    <button type="button" onClick={() => onStatusChange("planted")} className={`px-4 text-sm font-medium ${statusType === "planted" && "bg-tertiary"} rounded-l-2xl border-tertiary border-2`}>
+                    <button type="button" onClick={() => onStatusChange("planted")} className={`px-4 py-2 text-sm font-medium ${statusType === "planted" && "bg-tertiary"} rounded-l-2xl border-tertiary border-2`}>
                         Planted
                     </button>
-                    <button type="button" onClick={() => onStatusChange("harvested")} className={`px-4 text-sm font-medium ${statusType === "harvested" && "bg-tertiary"} border-tertiary border-2`}>
+                    <button type="button" onClick={() => onStatusChange("harvested")} className={`px-4 py-2 text-sm font-medium ${statusType === "harvested" && "bg-tertiary"} border-tertiary border-2`}>
                         Harvested
                     </button>
-                    <button type="button" onClick={() => onStatusChange("others")} className={`px-4 text-sm font-medium ${statusType === "others" && "bg-tertiary"} rounded-r-2xl border-tertiary border-2`}>
+                    <button type="button" onClick={() => onStatusChange("others")} className={`px-4 py-2 text-sm font-medium ${statusType === "others" && "bg-tertiary"} rounded-r-2xl border-tertiary border-2`}>
                         others
                     </button>
                 </div>
